694f362ad6a814d319a99c6d3fb87da4d37dc52c
Chrysanthos Prodromou <pchrysanthos@gmail.com>
2018-10-28 16:33:22 +0300

    Fixed bugs

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,8 @@ const settings = { timestampsInSnapshots: true };
 admin.firestore().settings(settings);
 
 const baseUrl = 'https://sandbox-apis.bankofcyprus.com/df-boc-org-sb/sb/psd2'
+const paymentSignBaseUrl = 'https://sandbox-apis.bankofcyprus.com/df-boc-org-sb/sb/jwssignverifyapi/sign'
+
 process.env.DEBUG = 'dialogflow:debug';
 
 const client_id = 'ee73f1b1-6ed4-4825-a27a-c944239f9cbe'
@@ -23,23 +25,6 @@ const tppid = 'singpaymentdata'
 const originUserId = '50520218'
 const journeyId = 'abc'
 const redirectURI = 'https://us-central1-chatbank-a2cdf.cloudfunctions.net/authenticate'
-
-exports.transferMoney = functions.https.onRequest((request, response) => {
-    if (!request.transferee) {
-        response.send({ error: 'Transferee not set!' })
-    }
-    if (!request.transferor) {
-        response.send({ error: 'Transferor not set!' })
-    }
-
-    let auth = 'AAIRbpPRb-zp58SpQLqiBXWKN_Q_Epvbndge8Sr-pMCwCKcZQduy-jkHwT_YqS0YyDHQOrVVPBoLq0RTuSjB_areNysc9n0GisbFPp--oxGc9g'
-
-    const url = baseUrl + '/oauth2/authorize?response_type=code' +
-        '&redirect_uri=https://us-central1-chatbank-a2cdf.cloudfunctions.net/authenticate' +
-        '&scope=UserOAuth2Security' +
-        '&client_id=ee73f1b1-6ed4-4825-a27a-c944239f9cbe' +
-        '&subscriptionid=Subid000001-1540638452780'
-})
 
 async function getCompleteToken(code) {
 
@@ -63,22 +48,14 @@ async function getCompleteToken(code) {
         url,
     };
 
-    let response2 = undefined
-    try {
-        response2 = await axios(options)
-    } catch (e) {
-        console.log('errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr')
-        console.log((e.response.data.error.additionalDetails))
-        return 'error'
-    }
+    let response2 = await axios(options)
+
     let access_token = response2.data.access_token
 
-    console.log('sabing', access_token)
     await admin.firestore().collection('users').doc('boc1').update({
         'access_token_complete_oauth': access_token
     })
 
-    console.log('testing saving')
     return access_token
 }
 
@@ -190,7 +167,7 @@ async function getToken() {
     return access_token
 }
 
-async function refreshToken() {
+async function refreshTokenCode() {
     let url = baseUrl + '/oauth2/token'
 
     const headers = {
@@ -290,7 +267,7 @@ async function getLoginUrl() {
 exports.readPlates = functions.https.onRequest(async (request, response) => {
     let plate = request.query.plate
 
-    await admin.firestore().collection('users').doc('boc1').set({
+    await admin.firestore().collection('users').doc('boc1').update({
         'plates': plate
     })
 
@@ -312,16 +289,148 @@ exports.authenticate = functions.https.onRequest(async (request, response) => {
 
     let access_token = await getCompleteToken(code)
 
-
     let user_doc = await admin.firestore().collection('users').doc('boc1').get()
 
     let accounts = await retrieveSubscriptionAccounts(user_doc.data().subscriptionId, user_doc.data().access_token)
 
-    let aaaa = await patchSubscription(user_doc.data().subscriptionId, accounts, access_token)
+    let sub = await patchSubscription(user_doc.data().subscriptionId, accounts, access_token)
 
-    response.send('<html><body><script>window.close();</script></body></html>')
+    response.send('<html><body>Login successful!<script>window.close();</script></body></html>')
 })
 
+// Step 3 - Transfer Money
+exports.transferMoney = functions.https.onRequest(async (request, response) => {
+    if (!request.query.transferee) {
+        response.send({ error: 'Transferee not set!' })
+    }
+
+    if (!request.query.transferor) {
+        response.send({ error: 'Transferor not set!' })
+    }
+
+    if (!request.query.amount) {
+        response.send({ error: 'Amount not set!' })
+    }
+
+
+    response.send(await transferMoneyTo(request.query.transferee, request.query.transferor, request.query.amount))
+})
+async function transferMoneyTo(transferee, transferor, amount) {
+    let user_doc = await admin.firestore().collection('users').doc('boc1').get()
+
+    let payload = await signPayment(transferee, transferor, amount)
+
+    let paymentId = await createPayment(payload, user_doc.data().access_token, user_doc.data().subscriptionId)
+
+    let refNumber = await approvePayment(paymentId, user_doc.data().access_token, user_doc.data().subscriptionId)
+
+    return refNumber
+}
+async function signPayment(transferee, transferor, amount) {
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'tppId': tppid
+    };
+
+    const data = {
+        "debtor": {
+            "bankId": "",
+            "accountId": transferor
+        },
+        "creditor": {
+            "bankId": "",
+            "accountId": transferee
+        },
+        "transactionAmount": {
+            "amount": amount,
+            "currency": "EUR",
+            "currencyRate": "string"
+        },
+        "endToEndId": "string",
+        "paymentDetails": "test sandbox ",
+        "terminalId": "string",
+        "branch": "",
+        "executionDate": "",
+        "valueDate": ""
+    }
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        data: data,
+        url: paymentSignBaseUrl,
+    };
+
+    let response = await axios(options)
+
+    let payload = response.data
+
+    return payload
+}
+
+async function createPayment(payload, access_token, subscriptionId) {
+
+    const url = baseUrl + '/v1/payments?client_id=' + client_id + '&client_secret=' + client_secret
+
+    const headers = {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json',
+        'tppid': tppid,
+        'originUserId': originUserId,
+        'timeStamp': + new Date(),
+        'journeyId': journeyId,
+        'subscriptionId': subscriptionId,
+        'lang': 'en',
+        'correlationId': 'xyz'
+    };
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        data: payload,
+        url,
+    };
+
+    let response = await axios(options)
+
+    let paymentId = response.data.payment.paymentId
+
+    return paymentId
+}
+
+async function approvePayment(paymentId, access_token, subscriptionId) {
+
+    const url = baseUrl + '/v1/payments/' + paymentId + '/authorize?client_id=' + client_id + '&client_secret=' + client_secret
+
+    const headers = {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json',
+        'tppid': tppid,
+        'originUserId': originUserId,
+        'timeStamp': + new Date(),
+        'journeyId': journeyId,
+        'subscriptionId': subscriptionId,
+    };
+
+    const data = {
+        "transactionTime": '1515051381394',
+        "authCode": "123456"
+    }
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        data: data,
+        url,
+    };
+
+    let response = await axios(options)
+
+    let refNumber = response.data.refNumber
+
+    return refNumber
+}
 
 async function getOurUser(subscriptionId, access_token) {
     let url = baseUrl + '/v1/accounts?client_id=' + client_id + '&client_secret=' + client_secret
@@ -344,6 +453,7 @@ async function getOurUser(subscriptionId, access_token) {
 
     let response2 = await axios(options)
 
+    // await admin.
     return response2.data[0]
 }
 
@@ -371,29 +481,39 @@ async function getBalanceForAccountId(accountId, subscriptionId, access_token) {
     return response2.data[0].balances[0].amount
 }
 
+function getAccountIdByName(name) {
+    return '351012345672'
+}
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({ request, response });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    // console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    // console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-    function transfer(agent) {
+    async function transfer(agent) {
+        await refreshTokenCode()
         let amount = request["body"]["queryResult"]["parameters"]["unit-currency"]["amount"]
         let name = request["body"]["queryResult"]["parameters"]["given-name"]
-        // agent.add('Response from Cloud functions - Your balance is ' + Math.random());
-        agent.add('Response from Cloud functions -' + 'Transfered ' + amount + ' to ' + name)
+
+        let transferor = (await admin.firestore().collection('users').doc('boc1').get()).data().accounts[0].accountId
+
+        let transferee = await getAccountIdByName(name)
+
+        let refNumber = await transferMoneyTo(transferee, transferor, amount)
+
+        agent.add('Your transfer of ' + amount + 'euros to account number '
+            + transferee + ' has been completed successfully! Your receipt number is #' + refNumber)
     }
 
     async function balance(agent) {
+        await refreshTokenCode()
         let user_doc = await admin.firestore().collection('users').doc('boc1').get()
 
-        console.log(user_doc)
         let access_token = user_doc.data().access_token
         let ourUser = (await getOurUser(user_doc.data().subscriptionId, access_token))
-        console.log('ourUser', ourUser)
 
         let balance = await getBalanceForAccountId(ourUser.accountId, user_doc.data().subscriptionId, access_token)
-        agent.add('Response from Cloud functions - Your balance is ' + balance);
+        agent.add('Your balance on account ' + ourUser.accountId + ' is ' + balance + ' euros.');
     }
 
     function fallback(agent) {
@@ -408,14 +528,19 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
 
     async function refreshToken(agent) {
-        await refreshToken()
+        await refreshTokenCode()
 
         agent.add('Token refreshed!')
+    }
+
+    async function goOut(agent) {
+        agent.add('You should not spend more than 500 or less than 200')
     }
 
     let intentMap = new Map();
     intentMap.set('Balance', balance);
     intentMap.set('Transfer', transfer);
+    intentMap.set('Go Out', goOut);
     intentMap.set('Default Fallback Intent', fallback);
     intentMap.set('Login', login)
     intentMap.set('RefreshToken', refreshToken)
